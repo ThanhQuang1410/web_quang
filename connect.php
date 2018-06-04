f77781a7e229ca4fb040d8e876bd6fdfcf964eb5
ThanhQuang1410 <ttquang1410@gmail.com>
2018-06-04 15:28:14 +0700

    commit

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
  <?php
 $servername = "localhost";
 $username = "root";
-$password = "23697";
+$password = "";
 $dbname = "shoestore";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
